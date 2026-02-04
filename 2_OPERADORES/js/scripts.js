1ecaeff58f3fb28b793c53f5dc5bb6e3999ad923
pacheckitos <matheuspacheco7319@gmail.com>
2026-02-04 12:08:52 -0300

    nocas operações

---
+++ 2_OPERADORES/js/scripts.js
@@ -0,0 +1,16 @@
+// 1 - Number
+console.log (typeof 2);
+console.log (typeof 2.2);
+console.log (typeof 4)
+
+//2 - Ops. aritméticas
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(4 * 5);
+console.log(10 / 2);
+console.log (5 + (4*12))
+
+// 3 - Special Numbers
+console.log(typeof Infinity);
+console.log(12 * "asd");
+console.log(typeof NaN);
