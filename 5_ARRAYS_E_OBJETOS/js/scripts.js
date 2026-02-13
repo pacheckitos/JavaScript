1ab96c31582508a1715f6afa11025cc78c5db50f
pacheckitos <matheuspacheco7319@gmail.com>
2026-02-13 09:54:11 -0300

    aaa

--- 5_ARRAYS_E_OBJETOS/js/scripts.js
+++ 5_ARRAYS_E_OBJETOS/js/scripts.js
@@ -1,39 +1,42 @@
 // 1 - Arrays
-const lista = [1, 2, 3 ,4 ,5];
+const lista = [1, 2, 3, 4, 5];
 
 console.log(lista);
 console.log(typeof lista);
 
-const itens2 = ["Roberto", true, 2, 4.12];
-console.log(itens2);
-
-// 1.a - ** Adicionando itens **
+const itens = ["Roberto", true, 2, 4.12];
+console.log(itens);
+// 1 - A - *** Adicionando itens ***
 lista.push(6);
 console.log(lista);
-// Método push adiciona o item ao final da lista
+// push(): Adiciona um item ao final da lista
 
 lista.unshift(0);
 console.log(lista);
-// Método unshift adiciona o item no início da lista
+// unshift: Adiciona um item ao início da lista
 
-// 1.b - ** Removendo itens **
+// 1 - B *** Removendo itens ***
+const itens2 = ["Roberto", true, 2, 4.12];
+console.log(itens2);
+
+// pop(): Remove o último item da lista
 itens2.pop();
 console.log(itens2);
-// Método pop remove o último item da lista
 
-
+//  shift(): Remove o  primeiro elemento da lista
 itens2.shift();
 console.log(itens2);
-// Método shift remove o primeiro elemento da lista
 
 // 2 - Mais sobre arrays
 const arr = ["a", "b", "c", "d"];
+
 console.log(arr[0]);
 console.log(arr[2]);
 console.log(arr[10]);
 
-// 3 - Propriedades
+// 3 - Propiedades
 const numbers = [5, 12, 4, 22];
+
 console.log(numbers.length);
 console.log(numbers["length"]);
 
@@ -44,26 +47,28 @@ const allNumbers = numbers.concat(otherNumbers);
 console.log(allNumbers);
 
 const text = "algum texto";
-console.log(text);
+
 console.log(text.toUpperCase());
-console.log(typeof text.toUpperCase());
+console.log(typeof text.toUpperCase);
 console.log(text.indexOf("g"));
 
 // 5 - Objetos - Object Literals
 const person = {
     name: "Roberto",
     age: 36,
-    job: "Programador"
+    job: "Programador",
 };
 
 console.log(person.name);
+console.log(person.name.length);
+console.log(typeof person);
 
 // 6 - Criando e deletando propriedades
 const car = {
     engine: 2.0,
-    brand: "Volkswagen",
+    brand: "VW",
     model: "Tiguan",
-    km: 20000
+    km: 20000,
 }
 
 console.log(car);
@@ -76,16 +81,16 @@ console.log(car);
 // 7 - Mais sobre objetos
 const obj = {
     a: "teste",
-    b: true
-}
-
-console.log(obj instanceof Object);
-
-const obj2 = {
-    c: []
+    b: true,
 };
 
-Object.assign(obj2, obj);
+console.log(obj instanceof Object); // verifica se retorna True
+
+const obj2 = {
+    c: [],
+};
+
+Object.assign(obj2, obj);//Copia um objeto para dentro de outro
 console.log(obj2);
 
 // 8 - Mais sobre objetos
@@ -97,24 +102,39 @@ console.log(Object.entries(obj));
 
 // 9 - Mutação
 const a = {
-    name: "Roberto"
+    name: "Roberto",
 };
 
 const b = a;
 
 console.log(a);
 console.log(b);
-console.log(b === a);
+console.log(a === b);
 
 a.age = 40;
-console.log(b)
+console.log(b);
+
+delete b.age;
 
 console.log(a);
-console.log( b);
+console.log(b);
+
 
 // 10 - Loops de arrays
 const users = ["Arthur", "Leonardo", "Lucas", "Matheus", "Yuri", "Willian"];
 
-for (let i =  0; i < users.length; i++){
-    console.log(`Listando usuário: ${users[i]}`)
-}
+for( let i=0; i < users.length; i++){
+    console.log(`Listando usuário: ${users[i]}`);
+};
+
+//11 - indexof e lastindexof
+const myElements = ["Morango", "Maçã", "Abacate", "Pêra", "Abacate"];
+
+console.log(myElements.indexOf("Maçã"));
+console.log(myElements.indexOf("Abacate"));
+
+console.log(myElements[2]);
+console.log(myElements[myElements.indexOf("Abacate")]);
+
+console.log(myElements.lastIndexOf("Abacate"));
+console.log();
